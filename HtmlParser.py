1b8754aaf0098f37fc2c3d6d9ee96d5f0b0192e9
ulrikson <erik.ulrikson@gmail.com>
2021-12-18 10:46:12 +0100

    importing kw extractor

--- HtmlParser.py
+++ HtmlParser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from Scraper import Scraper
-import re
+import yake
 
 
 class HtmlParser:
@@ -11,17 +11,33 @@ class HtmlParser:
         content = Scraper.getContent(self)
         cleanHtml = BeautifulSoup(content, "html.parser")
         return cleanHtml
-    
+
     def getTextRelatedTags(self):
-        headerTags = [f'h{x}' for x in range(1,7)]
-        otherTextTags = ['p']
+        headerTags = [f"h{x}" for x in range(1, 7)]
+        otherTextTags = ["p"]
         allTags = headerTags + otherTextTags
         return allTags
-    
+
     def getAllText(self):
         html = self.extractHtml()
         textTags = html.find_all(self.getTextRelatedTags())
-        return textTags
-    
+        text = [tag.text for tag in textTags]
+        return " ".join(text)
+
+    def generateKeyWords(self):
+        text = self.getAllText()
+        deduplicationThreshold = 0.9
+        language = "sv"
+        maxLengthOfKeyword = 2
+        numOfKeywords = 10
+        features = None
+
+        keywordExtractor = yake.KeywordExtractor(
+            lan=language, n=maxLengthOfKeyword, dedupLim=deduplicationThreshold, top=numOfKeywords, features=features
+        )
+        keywords = keywordExtractor.extract_keywords(text)
+
+        return keywords
+
     def test(self):
-        return self.getAllText()
+        return self.generateKeyWords()
